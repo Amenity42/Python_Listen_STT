ebe5f115fdbb8ce6bb5bb53a39a85acdd3538c8a
OddCompost <richomas@hotmail.com>
2023-08-30 14:19:16 +0100

    update_0.0.1

--- Key_phrase_activation.py
+++ Key_phrase_activation.py
@@ -13,37 +13,45 @@ def listen_and_print():
             audio = recognizer.listen(source)
 
             try:
-                text = recognizer.recognize_google(audio)
-                print("You said:", text)
+                  text = recognizer.recognize_google(audio)
+                  print("You said:", text)
+                  audio = recognizer.listen(source, timeout=5)  # Set timeout to 5 seconds
+                  
             except sr.UnknownValueError:
-                print("Sorry, could not understand audio")
+                  print("Sorry, could not understand audio")
             except sr.RequestError as e:
-                print("Error requesting results; {0}".format(e))
+                  print("Error requesting results; {0}".format(e))
+            except sr.WaitTimeoutError:
+                  print("Timeout: Nothing said.")
+                  listen_for_key_word()
 
-while True:
-    retries = 3  # Maximum number of retries
-    success = False
+def listen_for_key_word():
+      while True:
+            retries = 3  # Maximum number of retries
+            success = False
 
-    while retries > 0 and not success:
-        with sr.Microphone() as source:
-            print("Listening for key phrase...")
-            audio = recognizer.listen(source)
+            while retries > 0 and not success:
+                  with sr.Microphone() as source:
+                        print("Listening for key phrase...")
+                        audio = recognizer.listen(source)
 
-            try:
-                text = recognizer.recognize_google(audio)
-                print("You said:", text)
-                
-                if(text.lower() == "hello"):
-                    print("Key word detected")
-                    listen_and_print()  # Start listening and printing continuously
-                    success = True
-            except sr.RequestError as e:
-                print("Error requesting results; {0}".format(e))
-                break
-            except Exception as e:
-                print("Error:", e)
-                retries -= 1
-                print(f"Retries remaining: {retries}")
+                        try:
+                              text = recognizer.recognize_google(audio)
+                              print("You said:", text)
+                              
+                              if(text.lower() == "hello"):
+                                    print("Key word detected")
+                                    listen_and_print()  # Start listening and printing continuously
+                                    success = True
+                        except sr.RequestError as e:
+                              print("Error requesting results; {0}".format(e))
+                              break
+                        except Exception as e:
+                              print("Error:", e)
+                              retries -= 1
+                              print(f"Retries remaining: {retries}")
 
-        if not success:
-            print("Retrying...")
+                  if not success:
+                        print("Retrying...")
+
+listen_for_key_word() # Start listening for key phrase
